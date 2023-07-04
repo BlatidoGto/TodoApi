97acc0f9feb6e2cb520018347c42958e561968c8
DGTITFPEREZFL <DGTITFPEREZFL@DGTITFPEREZFLDE>
2023-07-04 13:38:00 -0600

    Agregar archivos de proyecto.

---
+++ TodoApi/Models/TodoContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace TodoApi.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContext<TodoContext> options)
+            : base(options) 
+        {
+
+        }
+
+        public DbSet<TodoContext> TodoItems { get; set; } = null!;
+    }
+}
